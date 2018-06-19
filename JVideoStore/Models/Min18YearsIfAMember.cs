e19cc279e06bfdadb3fcfa084c44cbaca100e31f
Jaime Oliveira <jaimepn@gmail.com>
2018-06-19 19:58:19 +0100

    Used ValidationAttribute to validate if customer is +18 when enrolling in membership

---
+++ JVideoStore/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JVideoStore.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("Birthdate is Required.");
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            if (age >= 18)
+                return ValidationResult.Success;
+            else
+                return new ValidationResult("Customer has to be at least 18 years old to enroll in membership.");
+
+        }
+
+    }
+}
