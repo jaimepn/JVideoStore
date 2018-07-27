df3e09c6a5ad9e29771265084d597e49d5625734
Jaime Oliveira <jaimepn@gmail.com>
2018-07-27 19:03:54 +0100

    Implemented confirmation (bootbox) -  required to change bootstrap version

--- JVideoStore/App_Start/BundleConfig.cs
+++ JVideoStore/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace JVideoStore
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.ResetAll();
+            BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -21,6 +24,7 @@ namespace JVideoStore
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
